d6c9c7156e75185f2cb4d344b1d1f26566f8ef53
aishaathmanlali <aishaathmanal@gmail.com>
2023-09-30 15:03:42 +0100

    add naswers to level-200.js

--- js1-wk1-eval/level-200.js
+++ js1-wk1-eval/level-200.js
@@ -2,15 +2,20 @@
 // Part 1: Without running code,
 // Use documentation to evaluate Math.trunc() and Math.floor() for each of the numbers below.
 // Write your answers down below:
+The Math.trunc() static method returns the integer part of a number by removing any fractional digits.
+The Math.floor() static method always rounds down and returns the largest integer less than or equal to a given number.
 
 // a) 10.53   For example, Math.trunc(10.53) evaluates to 10, Math.floor(10.53) evaluates to 10
-// b) 170.105
+// b) 170.105 
+Math.trunc(170.105)= 170, Math.floor(170.105)= 170
 // c) -0.34
+Math.trunc(-0.34)= -0, Math.floor(-0.34)= -1
 // d) -8.921
+Math.trunc(-8.921)= -8, Math.floor(-8.921)= -9
 
 // Part 2: Use the Node REPL to check your answers to Part 1)
 // Part 3: Using Part 2, explain the difference between Math.trunc() and Math.floor() in your own words
-
+Math.trunc() cuts the decimal part and leave the whole number but Math.floor rounds down to the nearest interger.
 
 
 
