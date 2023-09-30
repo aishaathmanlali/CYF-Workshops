e4c37cbc0f2156cfb1dc310c6f2b2ce79850f5bd
aishaathmanlali <aishaathmanal@gmail.com>
2023-09-30 16:04:37 +0100

    add answers level-500.js

--- js1-wk1-eval/level-500.js
+++ js1-wk1-eval/level-500.js
@@ -1,7 +1,9 @@
 
 
 const avgDaysInYear = Math.floor(365 / 12);
-
+/*
 // Without running any code,
 // Predict and explain what the value of avgDaysInYear will evaluate to
+it is trying to calculate the days in one month
 // You can use documentation to look up what Math.floor does
+it rounds down to the nearest integer.
