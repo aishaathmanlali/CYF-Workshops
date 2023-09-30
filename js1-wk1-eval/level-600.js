b9b11c6d668e3706dac1ed07dbaf492947f5c76f
aishaathmanlali <aishaathmanal@gmail.com>
2023-09-30 16:09:30 +0100

    add answers level-600.js

--- js1-wk1-eval/level-600.js
+++ js1-wk1-eval/level-600.js
@@ -2,7 +2,10 @@
 // Go to https://www.datecalculator.org/day-of-the-year
 
 // Declare a variable dayNumberOfTheYear with the value from this website
+let dayNumberOfTheYear = 273;
 
 // Declare a new variable daysLeftInTheYear by forming an expression with dayNumberOfTheYear
+let daysLeftInTheYear = (365-dayNumberOfTheYear)
+console.log(daysLeftInTheYear);
 
 // Use console.log to check your answer
