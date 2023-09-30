8532e37ca5ebeb84f1687e693d335970d0b2bb08
aishaathmanlali <aishaathmanal@gmail.com>
2023-09-30 15:13:37 +0100

    add naswers to level-300.js

--- js1-wk1-eval/level-300.js
+++ js1-wk1-eval/level-300.js
@@ -8,5 +8,9 @@ const time = "21:49";
 const macTimeDisplay = `${day.slice(0,3)} ${date} ${month.slice(0,3)} ${time}`;
 
 // Before running code, answer the following - write down your answers:
+Fri 29 Sep 21:49
 // a) how many function calls are there in this file?
+one function call
 // b) predict and explain what macTimeDisplay will evaluate to. You can use documentation to look up slice
+Fri 29 Sep 21:49
+The slice() method of String values extracts a section of this string and returns it as a new string, without modifying the original string.
