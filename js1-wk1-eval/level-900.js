2051df518348ea20e4c03dfa2583bc67756d34af
aishaathmanlali <aishaathmanal@gmail.com>
2023-10-05 10:26:29 +0100

    add answer level-900.js

--- js1-wk1-eval/level-900.js
+++ js1-wk1-eval/level-900.js
@@ -3,6 +3,9 @@
 
 const mitchAge = 33;
 const grandFatherAge = 83;
+const myAge = 31;
 
 // Create a variable called ageGap and assign to it the difference between grandFatherAge and myAge
 // Log the value of ageGap to the console 
+
+const ageGap = (grandFatherAge-myAge)
