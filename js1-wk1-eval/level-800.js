465c4addd33fde497a1072783e3dac493b5e3165
aishaathmanlali <aishaathmanal@gmail.com>
2023-10-05 10:05:29 +0100

    add answer level-800.js

--- js1-wk1-eval/level-800.js
+++ js1-wk1-eval/level-800.js
@@ -5,6 +5,13 @@ We’re making it easier to control what you see on Facebook and Instagram,
 and sharing more info on how our AI systems rank content.`
 
 const updatedPost = post.replaceAll('Facebook','Meta');
-
+/*
 // Use documentation to work out what updatedPost will evaluate to.
 // Don't run any code for this task
+//This is what it will evaluate to:
+//const updatedPost = const post = 
+`Meta
+How AI Influences What You See on Meta and Instagram
+We’re making it easier to control what you see on Meta and Instagram, 
+and sharing more info on how our AI systems rank content.`
+The replaceAll function will replace all the word Facebook with Meta in the string of variable post.
