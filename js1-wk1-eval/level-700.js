8d3e72f1c7cc72ca59e192244725ba7efbb9d320
aishaathmanlali <aishaathmanal@gmail.com>
2023-10-05 10:01:46 +0100

    add answer level-700.js

--- js1-wk1-eval/level-700.js
+++ js1-wk1-eval/level-700.js
@@ -4,3 +4,4 @@ console.log(total);
 
 // Without running the code, predict and explain what total will evaluate to
 // Use the JS1 Week 1 prep to help you work
+//I think total will be 10,32. This is because the console.log function will print it as it is.
