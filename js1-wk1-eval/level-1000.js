44f1d8071b2ad89dd86008279d222f00e6a32505
aishaathmanlali <aishaathmanal@gmail.com>
2023-10-05 10:48:30 +0100

    add answer level-1000.js

--- js1-wk1-eval/level-1000.js
+++ js1-wk1-eval/level-1000.js
@@ -9,4 +9,8 @@ console.log(Math.abs);
 console.log(Math.abs());
 console.log(Math.abs(-100));
 
+/*
 // Run the file with Node to check your answer
+console.log(Math.abs); This will not give any result because there are no values given.
+console.log(Math.abs()); This will give NaN because the value brackets are empty.
+console.log(Math.abs(-100)); This will give an answer of 100.
