7b7c471258fcbc8f77c64d4efd46ce140893e334
aishaathmanlali <aishaathmanal@gmail.com>
2024-02-24 12:53:50 +0000

    create LOgo and BestPokemon components

--- react-pokedex/pokedex/src/App.jsx
+++ react-pokedex/pokedex/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Logo />
+      <BestPokemon />
+    </div>
+  );
 }
 
-export default App
+function Logo() {
+  const appName = "Aisha's Pokedex"
+  return (
+    <div>
+      <h1>Welcome to {appName}</h1>
+      <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/026.png" />
+    </div>
+  );
+}
+
+function BestPokemon() {
+  return (
+  <p>My favourite pokemon is Pikachu</p>
+  );
+}
+
+export default App;
