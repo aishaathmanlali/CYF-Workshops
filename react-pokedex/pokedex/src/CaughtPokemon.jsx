383f4e0ae0f13e9876755f8e2874cbd7ad9b43f0
aishaathmanlali <aishaathmanal@gmail.com>
2024-02-24 14:36:33 +0000

    add array of pokemons to be chosen randomly when button is clicked

--- react-pokedex/pokedex/src/CaughtPokemon.jsx
+++ react-pokedex/pokedex/src/CaughtPokemon.jsx
@@ -1,16 +1,30 @@
 import { useState } from "react";
 
 function CaughtPokemon(Props) {
-const [caught, catchPokemon] = useState(0);
+  const [caught, catchPokemon] = useState([]);
+  const [names, setNames] = useState([
+    "Pikachu",
+    "Squirtle",
+    "Balbasaur",
+    "Caterpie",
+    "Metapod",
+  ]);
+  const index = Math.floor(Math.random() * names.length);
   return (
     <div>
-      <p>Caught {caught} pokemon on {Props.date}</p>
-      <button onClick={()=> catchPokemon(caught+1)}>Catch Pokemon</button>
+      <p>
+        Caught {caught.length} pokemon on {Props.date}
+      </p>
+      <button onClick={() => catchPokemon(caught.concat(names[index]))}>
+        Catch Pokemon
+      </button>
+      <ul>
+        {caught.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
     </div>
   );
-
 }
-
-
 
 export default CaughtPokemon;
