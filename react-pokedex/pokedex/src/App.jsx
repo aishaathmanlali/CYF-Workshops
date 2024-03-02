2eef42183856ac8be10f94cc4620ad55ccd8bf50
aishaathmanlali <aishaathmanal@gmail.com>
2024-03-02 13:10:13 +0000

    create routes to the components

--- react-pokedex/pokedex/src/App.jsx
+++ react-pokedex/pokedex/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Logo from "./Logo.jsx";
 import BestPokemon from "./BestPokemon.jsx";
 import CaughtPokemon from "./CaughtPokemon.jsx";
@@ -6,13 +7,54 @@ import PokemonCity from "./PokemonCity.jsx";
 
 function App() {
   return (
-    <div>
-      <Logo appName="Aisha's Pokedex" logWhenClicked={logWhenClicked} />
-      <BestPokemon abilities = {["Anticipation", "Adaptability", "Run-Away", "Cute-Charm"]} />
-      <CaughtPokemon date = {new Date().toLocaleDateString()} />
-      <PokemonMovesSelector />
-      <PokemonCity />
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/logo">Logo </Link>
+        <Link to="/best-pokemon">BestPokemon </Link>
+        <Link to="/caught-pokemon">CaughtPokemon </Link>
+        <Link to="/pokemon-moves-selector">PokemonMovesSelector </Link>
+        <Link to="/pokemon-city">PokemonCity </Link>
+      </nav>
+      <Routes>
+        <Route
+          element={
+            <Logo appName="Aisha's Pokedex" logWhenClicked={logWhenClicked} />
+          }
+          path="/logo"
+        />
+        <Route
+          element={
+            <BestPokemon
+              abilities={[
+                "Anticipation",
+                "Adaptability",
+                "Run-Away",
+                "Cute-Charm",
+              ]}
+            />
+          }
+          path="/best-pokemon"
+        />
+        <Route
+          element={<CaughtPokemon date={new Date().toLocaleDateString()} />}
+          path="/caught-pokemon"
+        />
+        <Route
+          element={<PokemonMovesSelector />}
+          path="/pokemon-moves-selector"
+        />
+        <Route element={<PokemonCity />} path="/pokemon-city" />
+      </Routes>
+      {/* <div>
+        <Logo appName="Aisha's Pokedex" logWhenClicked={logWhenClicked} />
+        <BestPokemon
+          abilities={["Anticipation", "Adaptability", "Run-Away", "Cute-Charm"]}
+        />
+        <CaughtPokemon date={new Date().toLocaleDateString()} />
+        <PokemonMovesSelector />
+        <PokemonCity />
+      </div> */}
+    </BrowserRouter>
   );
 }
 
