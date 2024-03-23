c49df143e73bb8295ada302b7cf9be2fc9f1b7b4
aishaathmanlali <aishaathmanal@gmail.com>
2024-03-23 11:07:56 +0000

    save an album using post endpoint

--- crud/server.js
+++ crud/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const albumsData = require("./albums.json");
 const app = express();
+app.use(express.json());
 
 app.get("/albums", (req, res) => {
   res.send(albumsData);
@@ -12,6 +13,15 @@ app.get("/albums/:albumId", (req, res) => {
   res.send(album);
 });
 
+app.post("/albums", (req, res) => {
+  const newAlbum = req.body;
+  albumsData.push(newAlbum);
+  res.send("Album added successfully!");
+});
+
+app.delete("/albums/:albumID", (req, res) => {
+  console.log("DELETE /albums route");
+});
 
 
 app.listen(3000, () => {
