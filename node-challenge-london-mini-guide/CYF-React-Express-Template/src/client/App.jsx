accf567fb125395acfd2a83136c2ea4a757f96e3
aishaathmanlali <aishaathmanal@gmail.com>
2024-04-06 15:55:35 +0100

    add title, dropdown menu and categories buttons in client side

--- node-challenge-london-mini-guide/CYF-React-Express-Template/src/client/App.jsx
+++ node-challenge-london-mini-guide/CYF-React-Express-Template/src/client/App.jsx
@@ -1,21 +1,21 @@
-import cyfLogo from "/cyf.svg";
-
 function App() {
   return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-screen py-2 h-72 bg-gradient-to-r from-purple-600 to-indigo-600">
-        <header className="container mx-auto px-4 content-center ">
-          <h1 className="text-3xl font-bold text-indigo-50 ">
-            Vite React Express CYF
-          </h1>
-        </header>
-        <main className="container mx-auto px-4 content-center ">
-          <a href="https://curriculum.codeyourfuture.io" target="_blank">
-            <img src={cyfLogo} className="max-w-xs" alt="CYF logo" />
-          </a>
-        </main>
-      </div>
-    </>
+    <div>
+      <h1>LONDON MINI-GUIDE</h1>
+      <form>
+        <select>
+          <option value="main">Select a city</option>
+          <option value="Startford">Stratford</option>
+          <option value="Harrow">Harrow</option>
+          <option value="Heathrow">Heathrow</option>
+        </select>
+        <br />
+        <button id="pharmacies">Pharmacies</button>
+        <button id="hospitals">Hospitals</button>
+        <button id="doctors">Doctors</button>
+        <button id="colleges">Colleges</button>
+      </form>
+    </div>
   );
 }
 
