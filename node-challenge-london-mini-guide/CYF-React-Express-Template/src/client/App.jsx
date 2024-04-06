3cbaab3b2caf858246cd1d59aac48e81b8d4acb9
aishaathmanlali <aishaathmanal@gmail.com>
2024-04-06 16:36:44 +0100

    add css styles

--- node-challenge-london-mini-guide/CYF-React-Express-Template/src/client/App.jsx
+++ node-challenge-london-mini-guide/CYF-React-Express-Template/src/client/App.jsx
@@ -2,19 +2,18 @@ function App() {
   return (
     <div>
       <h1>LONDON MINI-GUIDE</h1>
-      <form>
+      <p>choose a city:</p>
         <select>
           <option value="main">Select a city</option>
           <option value="Startford">Stratford</option>
           <option value="Harrow">Harrow</option>
           <option value="Heathrow">Heathrow</option>
         </select>
-        <br />
-        <button id="pharmacies">Pharmacies</button>
-        <button id="hospitals">Hospitals</button>
-        <button id="doctors">Doctors</button>
-        <button id="colleges">Colleges</button>
-      </form>
+      <br />
+      <button id="pharmacies">Pharmacies</button>
+      <button id="hospitals">Hospitals</button>
+      <button id="doctors">Doctors</button>
+      <button id="colleges">Colleges</button>
     </div>
   );
 }
