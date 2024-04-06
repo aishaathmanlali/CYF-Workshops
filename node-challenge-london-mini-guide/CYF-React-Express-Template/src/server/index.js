037a6ca1c500ed9fd9366c444bc8d246d48b09f7
aishaathmanlali <aishaathmanal@gmail.com>
2024-04-06 12:07:38 +0100

    add route endpoint to stratford json

--- node-challenge-london-mini-guide/CYF-React-Express-Template/src/server/index.js
+++ node-challenge-london-mini-guide/CYF-React-Express-Template/src/server/index.js
@@ -1,10 +1,30 @@
 import express from "express";
-import heathrow from "./data/Heathrow.json" assert {type:"json"};
+import heathrow from "./data/Heathrow.json" assert { type: "json" };
+import stratford from "./data/Stratford.json" assert { type: "json" };
+import harrow from "./data/Harrow.json" assert { type: "json" };
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send('"/pharmacies": "returns an array of pharmacies in a specific area"')
+  res.send(
+    '{"/pharmacies": "returns an array of pharmacies in a specific area"}'
+  );
+});
+
+app.get("/pharmacies", (req, res) => {
+  res.send(stratford.pharmacies);
+});
+
+app.get("/colleges", (req, res) => {
+  res.send(stratford.colleges);
+});
+
+app.get("/doctors", (req, res) => {
+  res.send(stratford.doctors);
+});
+
+app.get("/hospitals", (req, res) => {
+  res.send(stratford.hospitals);
 });
 
 app.listen(4173, () => {
